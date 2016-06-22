d9b05b19e3ebccec8499524df1c0d7b006e156bb
Finn Årup Nielsen <fn@imm.dtu.dk>
2016-06-22 14:35:05 +0200

    Add requirements from requirements.txt

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@
 
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 setup(
     name='emokitten',
     description="Installation and setup configuration",
+    install_requires=requirements,
     py_modules=['emokitten'],
     entry_points={
         'console_scripts': [
